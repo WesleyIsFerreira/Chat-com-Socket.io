9572a8f30e3254d9e5c69d519ae54acb9a70b50a
Wesley Ferreira <37143147+WesleyIsFerreira@users.noreply.github.com>
2021-06-02 01:16:35 -0300

    Finalizado

--- server.js
+++ server.js
@@ -17,8 +17,17 @@ app.use('/', (req, res) =>{
     res.render('index.html');
 });
 
+let messages = [];
+
 io.on('connection', socket => {
     console.log(`Socket Conectado: ${socket.id}`);
+
+    socket.emit('previousMessages', messages);
+
+    socket.on('sendMessage', data => { 
+        messages.push(data);
+        socket.broadcast.emit('receivedMessage', data);
+    });
 });
 
 server.listen(3000);
